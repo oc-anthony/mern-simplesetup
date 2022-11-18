d09f15ed8442292c5ce43abae0fddf55b7b9f800
oc-anthony <oc.anthony.ii@gmail.com>
2022-11-19 00:08:54 +0800

    serve static files

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 import express from 'express'
 import devBundle from './devBundle'
+import path from 'path'
 
+const CURRENT_WORKING_DIR = process.cwd()
 const app = express()
 devBundle.compile(app)
+
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
 
